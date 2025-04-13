f080ffe1da7fcd737d09d2eb093fabc9d370953f
Richard <richardzhiyuanzhang@gmail.com>
2025-04-13 15:13:00 -0400

    create status.ts with helper function that checks if the letters of a guessed word in the correct positions.

---
+++ app/src/utils/status.ts
@@ -0,0 +1,27 @@
+import { WORD_LENGTH } from "./words";
+
+export function calculateGuessStatus(guess: string, solution: string) {
+    const statuses = Array(WORD_LENGTH).fill("absent") as string[];
+    const solutionLetters = solution.split("") as string[];
+    const guessLetters = guess.split("") as string[];
+    const letterCounts = {} as { [key: string]: number };
+
+    solutionLetters.forEach((letter: string) => {
+        letterCounts[letter] = (letterCounts[letter] || 0) + 1;
+    });
+
+    guessLetters.forEach((letter: string, index: number) => {
+        if (letter === solution[index]) {
+            statuses[index] = "correct";
+            letterCounts[letter]--;
+        }
+    });
+
+    guessLetters.forEach((letter: string, index: number) => {
+        if (statuses[index] === "correct") return;
+        if (solutionLetters.includes(letter) && letterCounts[letter] > 0) {
+            statuses[index] = "present";
+            letterCounts[letter]--;
+        }
+    }   );
+}
