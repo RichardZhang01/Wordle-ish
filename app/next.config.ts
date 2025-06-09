c0411f0a59c5847cf0a53f707471e1c6f19ff4ce
Richard <richardzhiyuanzhang@gmail.com>
2025-06-08 22:09:48 -0400

    prep deployment

--- app/next.config.ts
+++ app/next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   basePath: "/Wordle-ish",
+  assetPrefix: "/Wordle-ish/",
   output: "export",
   reactStrictMode: true,
 };
